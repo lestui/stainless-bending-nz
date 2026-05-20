dcc73abdf7c4af02ecfb339f0bfb024ee21def1a
Leslie Tuilaepa <spacificwoodworkcnc@gmail.com>
2026-05-20 17:03:21 +1200

    fix favicon path

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ export const metadata = {
   title: 'Stainless Bending NZ | Precision Tube Bending & Fabrication Auckland',
   description: 'Auckland\'s specialists in stainless steel tube bending, custom bends, and Supreme Grab Rails. Over 20 years experience. Penrose, Auckland.',
   icons: {
-    icon: '/images/icon.png',
+    icon: '/icon.png',
   },
 }
 
